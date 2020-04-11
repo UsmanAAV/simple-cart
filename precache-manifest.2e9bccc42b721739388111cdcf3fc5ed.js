43c1416d106c97775004b5bce256c2b12758cf7c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-11 22:06:46 +0000

    Deploy UsmanAAV/simple-cart to github.com/UsmanAAV/simple-cart.git:gh-pages

---
+++ precache-manifest.2e9bccc42b721739388111cdcf3fc5ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95109153c5784ca00f2c",
+    "url": "/simple-cart/static/css/main.27936056.chunk.css"
+  },
+  {
+    "revision": "95109153c5784ca00f2c",
+    "url": "/simple-cart/static/js/main.c02ce1bf.chunk.js"
+  },
+  {
+    "revision": "e057dce25e873fd785c4",
+    "url": "/simple-cart/static/js/runtime~main.faac7a34.js"
+  },
+  {
+    "revision": "0858d51f6a702dd7479c",
+    "url": "/simple-cart/static/js/2.936e8a6d.chunk.js"
+  },
+  {
+    "revision": "789a32513b1f4b00cfb2cdd941be0778",
+    "url": "/simple-cart/index.html"
+  }
+];
