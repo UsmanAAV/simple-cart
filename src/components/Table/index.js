2b491c1fba4db6ddd04d8ef5c315fbe872095840
Azamat Abdullin <abdullin.azamat@clover.global>
2020-04-12 00:15:26 +0300

    added data-test attributes

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import './index.css';
 
-const TD = props => <td className="table-cell">{props.children}</td>;
+const TD = (props) => {
+  const { children, "data-test": dataTest } = props;
+  return (
+    <td
+      className="table-cell"
+      data-test={dataTest}
+    >
+      {children}
+    </td>
+  );
+};
 
 const Table = props => {
   const { items, cartEmptyMessage } = props;
@@ -18,10 +28,14 @@ const Table = props => {
           </thead>
           <tbody>
             {items.map((product, index) => (
-              <tr key={product.id} className={index % 2 === 0 ? 'evenrow' : 'oddrow'}>
+              <tr
+                key={product.id}
+                className={index % 2 === 0 ? 'evenrow' : 'oddrow'}
+                data-test={`row-${index}`}
+              >
                 <TD>{product.name}</TD>
                 <TD>{product.price}</TD>
-                <TD>{product.discountPrice}</TD>
+                <TD data-test="discount-price">{product.discountPrice}</TD>
               </tr>
             ))}
           </tbody>
